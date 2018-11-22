9d71c975b857db6b78842ed1e2182f961be3a089
PERSONALE\orazio.battaglia <orazio.battaglia@unibo.it>
2018-11-22 16:57:00 +0100

    Added scripts for windows

---
+++ files/ChangeWorkspaceAndKey.ps1
@@ -0,0 +1,23 @@
+﻿param (
+  [String]
+  $new_workspaceId,
+  [String]
+  $new_workspaceKey
+)
+
+$mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
+
+# Remove old workspaces
+$old_workspaces = $mma.GetCloudWorkspaces()
+
+foreach ($old_workspace in $old_workspaces) {
+  Write-Output $old_workspace.workspaceId
+  #$mma.RemoveCloudWorkspace($old_workspace.workspaceId)
+}
+
+$mma.ReloadConfiguration()
+
+# Add new workspace and key
+
+$mma.AddCloudWorkspace($new_workspaceId,$new_workspaceKey)
+$mma.ReloadConfiguration()
